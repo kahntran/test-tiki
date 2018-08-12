3c75cfcd9683566f95c36317a4bc7d5f3c0b76b1
khanhtran5555 <khanh.tran.5192@gmail.com>
2018-08-12 23:42:13 +0700

    :+1: Add more detail into README.md. Remove HTML code in index.php

--- README.md
+++ README.md
@@ -14,24 +14,45 @@
 5. Install Composer (and PHPUnit) into project
     * Connect to Docker Container 'web': docker-composer exec web bash
     * Install Composer (and PHPUnit): composer install
+    * Create file autoload: composer dump-autoload
 
 6. Testing
-    * On browser: http://localhost:8100 to access Web.
-    * On Terminal: ./vendor/bin/phpunit
+    * PHPUnit, use command: ./vendor/bin/phpunit
+    * PHP script, use command: php index.php
 
 7. Review result.
 
 # Tiki Home Test
 * This is a Tiki Home Test project.
 
+Task:
+
+Write a basic eCommerce program in PHP that models a User, Product and ShoppingCart.
+* Products have a name and price.
+* Users have a name and email address, and associated ShoppingCart.
+* ShoppingCarts have an associated User and list of Products.
+* A User can add or remove a Product from their ShoppingCart.
+* A ShoppingCart must be able to provide a total price for its list of Products.
+The code must pass two tests:
+1. Create a User "John Doe" with email address "john.doe@example.com", then add 2 "Apple" Products
+for $4.95 each and 1 "Orange" Product for $3.99. Check that the ShoppingCart total price is correct.
+2. Create a User "John Doe" with email address "john.doe@example.com", then add 3 "Apple" products
+for $4.95, then remove 1 "Apple" product. Check that the ShoppingCart total price is correct.
+You may either use PHPUnit to run the tests, or a simple PHP script which can be run via the command
+line.
+Notes:
+* You should NOT use a full-featured framework (Laravel, CakePHP, etc), just plain PHP code.
+* No database, user interaction or other functionality is required (so no HTML/CSS/JavaScript); it just
+needs to be PHP code which runs and passes the tests.
+
 # Requirements
-* Docker: php, apache, composer, phpunit
+* Docker: php, apache, composer
 
 # Assumption description
 * 1 User has only 1 Shopping Cart :
     - When a user is already initialized then you can not initialize another user.
     - When initializing a Shopping Cart, you will have to assign a user: Create a user -> Create a Shopping Cart associated with the user.
 
-# Algorithm description
+# Design Pattern and programming paradigm
 * OOP (Encapsulation) & SOLID (Open/Close principle): private variables & function get(), set().
 * Singleton in User & Shopping Cart: private __construct() & public static getInstance(). 
--- index.php
+++ index.php
@@ -6,46 +6,42 @@ use App\models\User;
 use App\models\ShoppingCart;
 use App\models\Product;
 
-echo '<br/>' . '<h3 align="center">Tiki Home Test</h3>' . '<br/>';
+echo "\n" . 'Tiki Home Test' . "\n";
 
-echo '<br/>' . '################# Initialize #################' . '<br/>';
+echo "\n" . '################# Initialize #################' . "\n";
 
 $apple = new Product('Apple', 4.95);
-echo '<br/>';
-echo 'One Product is created :' . '<br/>';
-echo 'Name : $' . $apple->getName() . '<br/>';
-echo 'Price : $' . $apple->getPrice() . '<br/>';
+echo "\n";
+echo 'One Product is created :' . "\n";
+echo 'Name : $' . $apple->getName() . "\n";
+echo 'Price : $' . $apple->getPrice() . "\n";
 
 $orange = new Product('Orange', 3.99);
-echo '<br/>';
-echo 'One Product is created :' . '<br/>';
-echo 'Name : $' . $apple->getName() . '<br/>';
-echo 'Price : $' . $apple->getPrice() . '<br/>';
+echo "\n";
+echo 'One Product is created :' . "\n";
+echo 'Name : $' . $apple->getName() . "\n";
+echo 'Price : $' . $apple->getPrice() . "\n";
 
 $user = User::getInstance('John Doe', 'john.doe@example.com');
-echo '<br/>';
-echo 'One User is created :' . '<br/>';
+echo "\n";
+echo 'One User is created :' . "\n";
 $shoppingCart = ShoppingCart::getInstance($user);
-echo $user->getName() . '\'s Shopping Cart is created :' . '<br/>';
-echo 'Name : ' . $user->getName() . '<br/>';
-echo 'Email : ' . $shoppingCart->getUser()->getEmail() . '<br/>';
-echo 'Shopping Cart :' . '<br/>';
-echo '<pre>';
+echo $user->getName() . '\'s Shopping Cart is created :' . "\n";
+echo 'Name : ' . $user->getName() . "\n";
+echo 'Email : ' . $shoppingCart->getUser()->getEmail() . "\n";
+echo 'Shopping Cart :' . "\n";
 print_r($user->getShoppingCart()->getProducts());
-echo '</pre>';
 
-echo '<br/>' . '################# Case 1 #################' . '<br/>';
+echo "\n" . '################# Case 1 #################' . "\n";
 
 $user->getShoppingCart()->add($apple, 2);
 $user->getShoppingCart()->add($orange);
-echo '<br/>';
-echo 'Add Product is done :' . '<br/>';
-echo '<pre>';
+echo "\n";
+echo 'Add Product is done :' . "\n";
 print_r($user->getShoppingCart()->getProducts());
-echo '</pre>';
-echo 'Total price : ' . $user->getShoppingCart()->getTotalPrice() . '<br/>';
+echo 'Total price : ' . $user->getShoppingCart()->getTotalPrice() . "\n";
 
-echo '<br/>' . '################# Reset Singleton instance(s): User & Shopping Cart #################' . '<br/>';
+echo "\n" . '################# Reset Singleton instance(s): User & Shopping Cart #################' . "\n";
 
 $singletonUser = User::getInstance('John Doe', 'john.doe@example.com');
 $singletonShoppingCart = ShoppingCart::getInstance($singletonUser);
@@ -65,55 +61,49 @@ $instanceShoppingCart->setValue(null, null);
 $instanceUser->setAccessible(false);
 $instanceShoppingCart->setAccessible(false);
 
-echo '<br/>' . 'Reset Singleton instance(s) is done.' . '<br/>';
+echo "\n" . 'Reset Singleton instance(s) is done.' . "\n";
 
-echo '<br/>' . '################# Initialize #################' . '<br/>';
+echo "\n" . '################# Initialize #################' . "\n";
 
 $apple = new Product('Apple', 4.95);
-echo '<br/>';
-echo 'One Product is created :' . '<br/>';
-echo 'Name : $' . $apple->getName() . '<br/>';
-echo 'Price : $' . $apple->getPrice() . '<br/>';
+echo "\n";
+echo 'One Product is created :' . "\n";
+echo 'Name : $' . $apple->getName() . "\n";
+echo 'Price : $' . $apple->getPrice() . "\n";
 
 $orange = new Product('Orange', 3.99);
-echo '<br/>';
-echo 'One Product is created :' . '<br/>';
-echo 'Name : $' . $apple->getName() . '<br/>';
-echo 'Price : $' . $apple->getPrice() . '<br/>';
+echo "\n";
+echo 'One Product is created :' . "\n";
+echo 'Name : $' . $apple->getName() . "\n";
+echo 'Price : $' . $apple->getPrice() . "\n";
 
 $user = User::getInstance('John Doe', 'john.doe@example.com');
-echo '<br/>';
-echo 'One User is created :' . '<br/>';
+echo "\n";
+echo 'One User is created :' . "\n";
 $shoppingCart = ShoppingCart::getInstance($user);
-echo $user->getName() . '\'s Shopping Cart is created :' . '<br/>';
-echo 'Name : ' . $user->getName() . '<br/>';
-echo 'Email : ' . $shoppingCart->getUser()->getEmail() . '<br/>';
-echo 'Shopping Cart :' . '<br/>';
-echo '<pre>';
+echo $user->getName() . '\'s Shopping Cart is created :' . "\n";
+echo 'Name : ' . $user->getName() . "\n";
+echo 'Email : ' . $shoppingCart->getUser()->getEmail() . "\n";
+echo 'Shopping Cart :' . "\n";
 print_r($user->getShoppingCart()->getProducts());
-echo '</pre>';
 
-echo '<br/>' . '################# Case 2 #################' . '<br/>';
+echo "\n" . '################# Case 2 #################' . "\n";
 
 $user->getShoppingCart()->add($apple, 3);
 $user->getShoppingCart()->add($orange);
-echo '<br/>';
-echo 'Add Product is done :' . '<br/>';
-echo '<pre>';
+echo "\n";
+echo 'Add Product is done :' . "\n";
 print_r($user->getShoppingCart()->getProducts());
-echo '</pre>';
-echo 'Total price : ' . $user->getShoppingCart()->getTotalPrice() . '<br/>';
+echo 'Total price : ' . $user->getShoppingCart()->getTotalPrice() . "\n";
 
-echo '<br/>';
+echo "\n";
 $resultRemove = $shoppingCart->remove($apple);
 if ($resultRemove) {
-    echo 'Remove Product is done :' . '<br/>';
-    echo '<pre>';
+    echo 'Remove Product is done :' . "\n";
     print_r($shoppingCart->getProducts());
-    echo '</pre>';
-    echo 'Total price : ' . $user->getShoppingCart()->getTotalPrice() . '<br/>';
+    echo 'Total price : ' . $user->getShoppingCart()->getTotalPrice() . "\n";
 } else {
-    echo 'Cant remove Product ' . $apple->getName() . '<br/>';
+    echo 'Cant remove Product ' . $apple->getName() . "\n";
 }
 
-echo '<br/>' . '<h3 align="center">The End</h3>';
+echo "\n" . 'The End';
